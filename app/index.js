b9be9babeeebce4bf227ff1f450a640b1a7d3da4
Federico Gratti <federicogratti@gmail.com>
2019-11-22 17:38:54 -0300

    Added signature comparison

--- app/index.js
+++ app/index.js
@@ -1,15 +1,19 @@
-var express = require("express");
-var app = express();
-var childProcess = require('child_process');
-var githubUsername = 'fedegratti'
+const express = require("express");
+const childProcess = require('child_process');
+const crypto = require('crypto');
 
+const secret = process.env.GITHUB_SECRET_TOKEN;
+const prod_branch = 'master';
+
+var app = express();
 app.use(express.json())
 
 app.post("/webhooks/github", function (req, res) {
-    var sender = req.body.sender;
     var branch = req.body.ref;
 
-    if (branch.indexOf('master') > -1 && sender.login === githubUsername) {
+    // If came from github and prod branch was updated
+    if (is_signature_correct(req) && branch.indexOf(prod_branch) > -1) {
+        console.log('Updating server...');
         deploy(res);
     }
 })
@@ -22,6 +26,11 @@ function deploy(res) {
         }
         res.sendStatus(200)
     });
+}
+
+function is_signature_correct(req) {
+    var signature = 'sha1=' + crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex');
+    return signature === req.headers['x-hub-signature'];
 }
 
 app.get("/", function (req, res) {
